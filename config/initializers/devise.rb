15d4922c5958dd4627e29c0732dac5a85366cb23
筒井巧 <takumin476@gmail.com>
2018-07-02 09:38:59 +0900

    fix twitter omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,6 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
-  config.omniauth :twitter, "	tf1Jzj9KUcSrt0S00Ld76R1Ev", "4A87pT59RRQWTlcr0U5B0QsSha7Sc6M1jWAykeEdHJVhYOpXK5"
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -259,6 +258,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, "tf1Jzj9KUcSrt0S00Ld76R1Ev", "4A87pT59RRQWTlcr0U5B0QsSha7Sc6M1jWAykeEdHJVhYOpXK5"
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
